5d8e4ae91318590e4cb83e9bbb74f43108cab7f9
Fernando Honig <fernandohonig@users.noreply.github.com>
2020-03-29 17:04:17 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from 'https://static.wixstatic.com/media/831ed4_08ee39b83c714a6db25a479c40aba905~mv2.png';
 import './App.css';
 import { withAuthenticator } from 'aws-amplify-react'
 import Amplify, { Auth } from 'aws-amplify';
@@ -13,15 +13,15 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Thanks for becoming a customer. Click below to Sign the MSA and SO
           </p>
           <a
             className="App-link"
-            href="https://reactjs.org"
+            href="https://app.hellosign.com/s/EpvA0XWl"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Sign MSA
           </a>
         </header>
       </div>
